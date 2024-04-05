c8f24014fe41872b2e5d68019c63d1da58ee1027
LoveIsTheAnswer51 <159463796+LoveIsTheAnswer51@users.noreply.github.com>
2024-04-05 05:09:02 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,29 @@
+
+    const data = [];
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+
+main();
+}
+function generateRandomData() {
+    data.forEach(item => {
+function main() {
+    for (let i = 0; i < 10; i++) {
+
+    });
+    return data;
+    }
+}
+        data.push(Math.floor(Math.random() * 100) + 1);public class RandomDataGenerator {
+        for (int item : data) {
+        }
+        }
+}
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+    }
+        for (int i = 0; i < 10; i++) {
+
+
+            data[i] = (int) (Math.random() * 100) + 1;
